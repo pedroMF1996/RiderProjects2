d10b66d1d4b8a60725d54aa53f845ba1c6929e27
PEDROMARTINSFALLEIRO <pmfrp@hotmail.com>
2019-06-08 21:18:15 -0300

    Determine quais localidades devem ser escolhidas para abrigar os centros de distribui ̧c ̃ao, associando a cada uma delas os clientes que devem ser atendidos pelas mesmas, de modo que se minimize a distˆancia total percorrida para as entregas semanais.

--- testeRSA10/testeRSA10/Program.cs
+++ testeRSA10/testeRSA10/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
-using CSharp_easy_RSA_PEM;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
 
 namespace testeRSA10
 {
@@ -7,14 +13,36 @@ namespace testeRSA10
     {
         static void Main(string[] args)
         {
-            string privateKeyStr;
-            Console.WriteLine("Hello World!");
-            using (var rsa = Crypto.CreateRsaKeys(2048))
+            string s1;
+            
+            using (var rsa = new RSACryptoServiceProvider(2048))
             {
-                privateKeyStr = Crypto.ExportPrivateKeyToRSAPEM(rsa);
+                s1 = GetKeyString(rsa.ExportParameters(false));
+                Console.WriteLine(s1);
+                rsa.FromXmlString(FromJsonString(s1));
+                Console.WriteLine("hello word");
+                Console.WriteLine(rsa.Encrypt(Convert.FromBase64String("hello word"), false));
+                
+                
             }
 
-            Console.WriteLine(privateKeyStr);
+            Console.WriteLine(s1);
+        }
+        public static string GetKeyString(RSAParameters publicKey)
+        {
+            var sw = new StringWriter();
+                sw.Write(JsonConvert.SerializeObject(publicKey));
+            return sw.ToString();
+        }
+        
+        public static string FromJsonString(string jsonString)
+        {
+            var paramsJson = JsonConvert.DeserializeObject<RSAParameters>(jsonString);
+            StringWriter sw = new StringWriter();
+            XmlSerializer xs = new XmlSerializer(typeof(RSAParameters));
+            xs.Serialize(sw, paramsJson);
+
+            return sw.ToString();
         }
     }
 }
